aa0cce821fbac4979829de84f0d1bd1ef975c7cb
lenaaviles <lenaricabal@gmail.com>
2017-11-19 21:06:57 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    //group add limit
+    var maxGroup = 10;
+    
+    //add more fields group
+    $(".addMore").click(function(){
+        if($('body').find('.fieldGroup').length < maxGroup){
+            var fieldHTML = '<div class="jumbotron">\
+            <div class="form-group fieldGroup">'
+            +$(".fieldGroupCopy").html()+'</div> </div>';
+            $('body').find('.jumbotron:last').after(fieldHTML);
+        }else{
+            alert('Maximum '+maxGroup+' groups are allowed.');
+        }
+    });
+    
+    //remove fields group
+    $("body").on("click",".remove",function(){ 
+        $(this).parents(".jumbotron").remove();
+    });
+
+    $(".remove").click(function(){
+        if($('body').find('.fieldGroup').length > 1){
+            $('body').find('.jumbotron:last').remove();
+        }});
+});
