2cf31c335a6a5d8680989ea603a25d01d07b95c7
lenaaviles <lenaricabal@gmail.com>
2017-11-20 20:51:06 +0200

    console output

--- js/script.js
+++ js/script.js
@@ -23,11 +23,41 @@ $(document).ready(function(){
         if($('body').find('.fieldGroup').length > 1){
             $('body').find('.jumbotron:last').remove();
         }});
-
-        $("body").on("click",".btn-toggle",function(){
-        $(this).parents(".fieldGroup").find('.pallet').toggle();
-        $(this).parents(".fieldGroup").find('.parcel').toggle();
-    });
     
-
+    // Pallet/Parcel toggle
+    $("body").on("click",".btn-toggle",function(){
+        event.preventDefault();
+    $(this).parents(".fieldGroup").find('.pallet').toggle();
+    $(this).parents(".fieldGroup").find('.parcel').toggle();
+    }); 
+    
+    //console output
+    $(".submit").click(function(){
+        $('body').find('.fieldGroup').each(function(index) {            
+            var pallet = $(this).find('.pallet');
+            var parcel = $(this).find('.parcel');
+            var palletDisplay = pallet.css("display");
+            if (palletDisplay == "none") {  
+                console.log(index + ": PARCEL");                            
+                parcel.each(function(){
+                    var label = $( this ).find( "label" ).text();
+                    var value = $( this ).find( ".form-control" ).val();
+                    var units = $( this ).find( "span" ).text();
+                    if(label) {
+                        console.log(label + ' ' + value + ' ' + units);
+                    }
+                })
+            } else {               
+                console.log(index + ": PALLET");                            
+                pallet.each(function(){
+                    var label = $( this ).find( "label" ).text();
+                    var value = $( this ).find( ".form-control" ).val();
+                    var units = $( this ).find( "span" ).text();
+                    if(label) {
+                        console.log(label + ' ' + value + ' ' + units);
+                    }
+                })
+            }
+    });
+});
 });
